cfd1ece97d0bcb074992cf2fb89bea3f09918fcc
Angus <angus.goody@gmail.com>
2023-10-14 16:40:55 +0100

    Added example table to ui

--- resources/js/Components/_util/PageContainer.vue
+++ resources/js/Components/_util/PageContainer.vue
@@ -4,7 +4,7 @@
 <!--    </div>-->
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-5">
             <slot/>
         </div>
     </div>
--- resources/js/Components/calendar/HistoryCalendar.vue
+++ resources/js/Components/calendar/HistoryCalendar.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <h3 class="h3">Your entries</h3>
+        <h2 class="h3">14th October</h2>
         <div class="lg:grid lg:grid-cols-12 lg:gap-x-16">
-            <div class="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-9">
+
+            <!-- Calendar -->
+            <div class="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-8">
 
                 <div class="flex items-center text-gray-900">
                     <button type="button" @click="previousMonth" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
@@ -32,53 +34,53 @@
                 </div>
                 <button type="button" class="mt-8 w-full rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600" @click="goToToday">Go to Today</button>
             </div>
-            <ol class="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8">
-                <li v-for="meeting in meetings" :key="meeting.id" class="relative flex space-x-6 py-6 xl:static">
-                    <img :src="meeting.imageUrl" alt="" class="h-14 w-14 flex-none rounded-full" />
-                    <div class="flex-auto">
-                        <h3 class="pr-10 font-semibold text-gray-900 xl:pr-0">{{ meeting.name }}</h3>
-                        <dl class="mt-2 flex flex-col text-gray-500 xl:flex-row">
-                            <div class="flex items-start space-x-3">
-                                <dt class="mt-0.5">
-                                    <span class="sr-only">Date</span>
-                                    <CalendarIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </dt>
-                                <dd>
-                                    <time :datetime="meeting.datetime">{{ meeting.date }} at {{ meeting.time }}</time>
-                                </dd>
-                            </div>
-                            <div class="mt-2 flex items-start space-x-3 xl:ml-3.5 xl:mt-0 xl:border-l xl:border-gray-400 xl:border-opacity-50 xl:pl-3.5">
-                                <dt class="mt-0.5">
-                                    <span class="sr-only">Location</span>
-                                    <MapPinIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </dt>
-                                <dd>{{ meeting.location }}</dd>
-                            </div>
-                        </dl>
-                    </div>
-                    <Menu as="div" class="absolute right-0 top-6 xl:relative xl:right-auto xl:top-auto xl:self-center">
-                        <div>
-                            <MenuButton class="-m-2 flex items-center rounded-full p-2 text-gray-500 hover:text-gray-600">
-                                <span class="sr-only">Open options</span>
-                                <EllipsisHorizontalIcon class="h-5 w-5" aria-hidden="true" />
-                            </MenuButton>
-                        </div>
 
-                        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                            <MenuItems class="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <div class="py-1">
-                                    <MenuItem v-slot="{ active }">
-                                        <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Edit</a>
-                                    </MenuItem>
-                                    <MenuItem v-slot="{ active }">
-                                        <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Cancel</a>
-                                    </MenuItem>
-                                </div>
-                            </MenuItems>
-                        </transition>
-                    </Menu>
-                </li>
-            </ol>
+            <!-- Timsheet section -->
+            <div class="mt-10 lg:col-span-7 xl:col-span-7">
+
+                <!-- Day stats -->
+                <div class="py-5 text-center">
+                   <p>Total worked time: 8h 15</p>
+                </div>
+                <!-- Entries table -->
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Clock in
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Clock out
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Duration
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="entry in entries" :key="entry.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                <td scope="row" class="px-6 py-4 ">
+                                    {{ entry.start }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ entry.end }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ entry.duration }}
+                                </td>
+                                <td class="px-6 py-4 text-right">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -94,18 +96,19 @@ import {
 } from '@heroicons/vue/20/solid'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
-const meetings = [
+const entries = [
     {
         id: 1,
-        date: 'January 10th, 2022',
-        time: '5:00 PM',
-        datetime: '2022-01-10T17:00',
-        name: 'Leslie Alexander',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        location: 'Starbucks',
+        start: '8:00',
+        end: '12:00',
+        duration: '4h'
     },
-    // More meetings...
+    {
+        id: 2,
+        start: '12:45',
+        end: '17:00',
+        duration: '4h 15m'
+    },
 ]
 const today = ref(new Date());
 const currentMonth = ref(today.value.getMonth());

1ad8d0d8e7b1da8eaa3b4d989c4d508a602c3ab0
Angus <angus.goody@gmail.com>
2023-10-15 21:40:38 +0100

    Passing Feature tests

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TimeRecordCollection;
+use App\Models\TimeRecord;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class HistoryController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('History', []);
+        $date = $request->input('date', now()->format('Y-m-d'));
+        $userId = Auth::user()->employee->id;
+
+        $timeRecords = TimeRecord::whereDate('recorded_at', $date)
+            ->where('employee_id', $userId)
+            ->get();
+
+        $timeRecordsResource = new TimeRecordCollection($timeRecords);
+
+        return Inertia::render('History', [
+            'timeRecords' => $timeRecordsResource
+        ]);
     }
 }
--- tests/Feature/TimeRecordTest.php
+++ tests/Feature/TimeRecordTest.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use App\Models\Employee;
 use App\Models\TimeRecord;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
+use Inertia\Testing\AssertableInertia as Assert;
 
 class TimeRecordTest extends TestCase
 {
@@ -56,6 +58,56 @@ class TimeRecordTest extends TestCase
             'type' => TimeRecord::CLOCK_OUT,
             'recorded_at' => now()->toDateTimeString()
         ]);
+    }
+
+    public function test_resource_correctly_organizes_clock_in_and_clock_out_entries()
+    {
+        $employee = Employee::factory()->create();
+        $this->actingAs($employee->user);
+
+        // Insert some records into the database
+        TimeRecord::create([
+            'employee_id' => $employee->id,
+            'type' => 'clock_in',
+            'recorded_at' => Carbon::parse('2023-04-15 09:00:00')
+        ]);
+
+        TimeRecord::create([
+            'employee_id' => $employee->id,
+            'type' => 'clock_out',
+            'recorded_at' => Carbon::parse('2023-04-15 13:00:00')
+        ]);
+
+        TimeRecord::create([
+            'employee_id' => $employee->id,
+            'type' => 'clock_in',
+            'recorded_at' => Carbon::parse('2023-04-15 14:00:00')
+        ]);
+
+        TimeRecord::create([
+            'employee_id' => $employee->id,
+            'type' => 'auto_clock_out',
+            'recorded_at' => Carbon::parse('2023-04-15 18:00:00')
+        ]);
+
+        // Make an HTTP request to the desired endpoint
+        $response = $this->get(route('history', ['date' => '2023-04-15']));
+
+        // Assert the Inertia response
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('History')
+            ->has('timeRecords.data', 2)
+            ->where('timeRecords.data.0.clock_in', '2023-04-15 09:00:00')
+            ->where('timeRecords.data.0.clock_out', '2023-04-15 13:00:00')
+            ->where('timeRecords.data.0.duration', '04:00:00')
+            ->where('timeRecords.data.0.ongoing', false)
+            ->where('timeRecords.data.0.auto_clock_out', false)
+            ->where('timeRecords.data.1.clock_in', '2023-04-15 14:00:00')
+            ->where('timeRecords.data.1.clock_out', '2023-04-15 18:00:00')
+            ->where('timeRecords.data.1.duration', '04:00:00')
+            ->where('timeRecords.data.1.ongoing', false)
+            ->where('timeRecords.data.1.auto_clock_out', true)
+        );
     }
 
 

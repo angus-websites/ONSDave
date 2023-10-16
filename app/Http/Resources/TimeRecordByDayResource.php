c1f4a7f6ff4d9844f0a0a75eb31f8d831b86ab8e
Angus <angus.goody@gmail.com>
2023-10-16 19:03:39 +0100

    Added new feature test for byDayResource

--- app/Http/Resources/TimeRecordByDayResource.php
+++ app/Http/Resources/TimeRecordByDayResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Carbon\Carbon;
 
 class TimeRecordByDayResource extends ResourceCollection
 {
---
+++ tests/Feature/Resources/TimeRecordByDayResourceTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Tests\Feature\Resources;
+
+use App\Http\Resources\TimeRecordByDayResource;
+use App\Models\Employee;
+use App\Models\TimeRecord;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+
+class TimeRecordByDayResourceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test the resource with four records
+     * which should result in two sessions
+     */
+    public function test_resource_organises_four_records_with_auto_clock_out(): void
+    {
+        // Mockup some fake records
+        $records = new Collection([
+            (object) [
+                'type' => 'clock_in',
+                'recorded_at' => ('2023-04-15 09:00:00')
+            ],
+            (object) [
+                'type' => 'clock_out',
+                'recorded_at' => ('2023-04-15 13:00:00')
+            ],
+            (object) [
+                'type' => 'clock_in',
+                'recorded_at' => ('2023-04-15 14:00:00')
+            ],
+            (object) [
+                'type' => 'auto_clock_out',
+                'recorded_at' => ('2023-04-15 18:00:00')
+            ]
+        ]);
+
+        // Create a date to pass to the resource
+        $date = Carbon::parse('2023-04-15');
+
+        // Pass the records to the resource
+        $resourceResult = (new TimeRecordByDayResource($records, $date))->toArray(request());
+
+        // Assert the date is correct
+        $this->assertEquals('2023-04-15', $resourceResult['date']);
+
+        // Fetch the records from the resource
+        $resourceResult = $resourceResult['records'];
+
+        // Assert the records are correctly organized
+        $this->assertEquals('2023-04-15 09:00:00', $resourceResult[0]['clock_in']);
+        $this->assertEquals('2023-04-15 13:00:00', $resourceResult[0]['clock_out']);
+        $this->assertEquals('04:00:00', $resourceResult[0]['duration']);
+        $this->assertFalse($resourceResult[0]['ongoing']);
+        $this->assertFalse($resourceResult[0]['auto_clock_out']);
+
+        // Assert the next record is correctly organized
+        $this->assertEquals('2023-04-15 14:00:00', $resourceResult[1]['clock_in']);
+        $this->assertEquals('2023-04-15 18:00:00', $resourceResult[1]['clock_out']);
+        $this->assertEquals('04:00:00', $resourceResult[1]['duration']);
+        $this->assertFalse($resourceResult[1]['ongoing']);
+
+    }
+
+    /**
+     * Test the resource with a single record
+     * which should result in a single session that is ongoing
+     */
+    public function test_resource_organises_records_with_ongoing_session(): void
+    {
+        // Mockup some fake records
+        $records = new Collection([
+            (object) [
+                'type' => 'clock_in',
+                'recorded_at' => ('2023-04-15 09:00:00')
+            ],
+        ]);
+
+        // Create a date to pass to the resource
+        $date = Carbon::parse('2023-04-15');
+
+        // Pass the records to the resource
+        $resourceResult = (new TimeRecordByDayResource($records, $date))->toArray(request());
+
+        // Assert the date is correct
+        $this->assertEquals('2023-04-15', $resourceResult['date']);
+
+        // Fetch the records from the resource
+        $resourceResult = $resourceResult['records'];
+
+        // Assert the records are correctly organized
+        $this->assertEquals('2023-04-15 09:00:00', $resourceResult[0]['clock_in']);
+        $this->assertNull($resourceResult[0]['clock_out']);
+        $this->assertNull($resourceResult[0]['duration']);
+        $this->assertTrue($resourceResult[0]['ongoing']);
+
+    }
+
+    /**
+     * Test the resource some missing data
+     * the clock_out records are missing
+     */
+    public function test_resource_with_some_missing_data(): void
+    {
+        // Mockup some fake records
+        $records = new Collection([
+            (object) [
+                'type' => 'clock_in',
+                'recorded_at' => ('2023-04-15 09:00:00')
+            ],
+            (object) [
+                'type' => 'clock_in',
+                'recorded_at' => ('2023-04-15 14:00:00')
+            ]
+        ]);
+
+        // Create a date to pass to the resource
+        $date = Carbon::parse('2023-04-15');
+
+        // Pass the records to the resource
+        $resourceResult = (new TimeRecordByDayResource($records, $date))->toArray(request());
+
+        // Assert the date is correct
+        $this->assertEquals('2023-04-15', $resourceResult['date']);
+
+        // Fetch the records from the resource
+        $resourceResult = $resourceResult['records'];
+
+        // Assert the records are correctly organized
+        $this->assertEquals('2023-04-15 09:00:00', $resourceResult[0]['clock_in']);
+        $this->assertNull($resourceResult[0]['clock_out']);
+        $this->assertNull($resourceResult[0]['duration']);
+        $this->assertTrue($resourceResult[0]['ongoing']);
+
+        // Assert the next record is correctly organized
+        $this->assertEquals('2023-04-15 14:00:00', $resourceResult[1]['clock_in']);
+        $this->assertNull($resourceResult[1]['clock_out']);
+        $this->assertNull($resourceResult[1]['duration']);
+        $this->assertTrue($resourceResult[1]['ongoing']);
+
+    }
+
+
+}

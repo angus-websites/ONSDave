7b2285ff89768e733c37b25b8d533edf0f43a18d
Angus <angus.goody@gmail.com>
2023-11-11 20:47:10 +0000

    Improved flash messages

--- resources/js/Components/FlashMessages.vue
+++ resources/js/Components/FlashMessages.vue
@@ -1,135 +1,77 @@
 <template>
-    <div>
-        <!-- Success flash messages -->
-        <div
-            v-if="$page.props.flash.success && show"
-            class="rounded-md bg-green-50 border border-green-700 p-4"
-        >
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <CheckCircleIcon
-                        class="h-5 w-5 text-green-400"
-                        aria-hidden="true"
-                    />
-                </div>
-                <div class="ml-3">
-                    <p class="text-sm font-medium text-green-800">
-                        {{ $page.props.flash.success }}
-                    </p>
-                </div>
-                <div class="ml-auto pl-3">
-                    <div class="-mx-1.5 -my-1.5">
-                        <button
-                            @click="show = false"
-                            type="button"
-                            class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-                        >
-                            <span class="sr-only">Dismiss</span>
-                            <XMarkIcon class="h-5 w-5" aria-hidden="true" />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="my-5">
+    <!-- Success flash messages -->
+    <MultiMessage
+      v-if="showSuccess && $page.props.flash.success"
+      @dismiss="showSuccess = false"
+      type="success"
+      :message="$page.props.flash.success"
+    />
 
-        <!-- Error flash messages -->
-        <div
-          v-if="
-                ($page.props.flash.error ||
-                    Object.keys($page.props.errors).length > 0) &&
-                show &&
-                !hideErrors
-            "
-          class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-          <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-          </svg>
-          <span class="sr-only">Error</span>
-
-
-          <div v-if="$page.props.errors">
-            <span class="font-medium">Errors...</span>
-            <ul class="mt-1.5 list-disc list-inside">
-              <li
-                v-for="(error, index) in $page.props.errors"
-                :key="index"
-              >
-                {{ error }}
-              </li>
-            </ul>
-          </div>
-          <div v-else-if="$page.props.flash.error">
-            <span class="font-medium">{{ $page.props.flash.error }}</span>
-          </div>
-        </div>
-
-        <!-- Info messages -->
-        <div
-            v-if="$page.props.flash.info && show"
-            class="rounded-md border border-blue-700 bg-blue-50 p-4"
-        >
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <InformationCircleIcon
-                        class="h-5 w-5 text-blue-400"
-                        aria-hidden="true"
-                    />
-                </div>
-                <div class="ml-3">
-                    <p class="text-sm text-blue-700">
-                        {{ $page.props.flash.info }}
-                    </p>
-                </div>
-                <div class="ml-auto pl-3">
-                    <div class="-mx-1.5 -my-1.5">
-                        <button
-                            @click="show = false"
-                            type="button"
-                            class="inline-flex rounded-md bg-blue-50 p-1.5 text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-blue-50"
-                        >
-                            <span class="sr-only">Dismiss</span>
-                            <XMarkIcon class="h-5 w-5" aria-hidden="true" />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <!-- Multi error messages -->
+    <div
+      v-if="showError && Object.keys($page.props.errors).length > 0 && !hideErrors"
+      class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      role="alert"
+    >
+      <!-- Error message content -->
+      <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <!-- SVG Path -->
+      </svg>
+      <span class="sr-only">Error</span>
+      <span class="font-medium">Errors...</span>
+      <ul class="mt-1.5 list-disc list-inside">
+        <li v-for="(error, index) in $page.props.errors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
+      <button @click="showError = false" class="ml-auto">Dismiss</button>
     </div>
+
+    <!-- Single error message -->
+    <MultiMessage
+      v-if="showErrorSingle && $page.props.flash.error && !hideErrors"
+      @dismiss="showErrorSingle = false"
+      type="danger"
+      :message="$page.props.flash.error"
+    />
+
+    <!-- Info messages -->
+    <MultiMessage
+      v-if="showInfo && $page.props.flash.info"
+      @dismiss="showInfo = false"
+      type="info"
+      :message="$page.props.flash.info"
+    />
+  </div>
 </template>
 
 <script>
-import {
-    CheckCircleIcon,
-    XMarkIcon,
-    XCircleIcon,
-    InformationCircleIcon,
-} from '@heroicons/vue/20/solid'
-export default {
-    props: {
-        hideErrors: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    components: {
-        CheckCircleIcon,
-        XMarkIcon,
-        XCircleIcon,
-        InformationCircleIcon,
-    },
+import MultiMessage from "./messages/MultiMessage.vue";
 
-    data() {
-        return {
-            show: true,
-        }
+export default {
+  props: {
+    hideErrors: {
+      type: Boolean,
+      default: false,
     },
-    watch: {
-        '$page.props.flash': {
-            handler() {
-                this.show = true
-            },
-            deep: true,
-        },
+  },
+  components: {
+    MultiMessage,
+    // Other components
+  },
+  data() {
+    return {
+      showSuccess: true,
+      showError: true,
+      showErrorSingle: true,
+      showInfo: true,
+    }
+  },
+  watch: {
+    '$page.props.flash': {
+      deep: true,
     },
+  },
 }
 </script>
--- resources/js/Components/messages/MultiMessage.vue
+++ resources/js/Components/messages/MultiMessage.vue
@@ -12,6 +12,12 @@
             <div class="ml-3 flex-1 md:flex md:justify-between">
                 <p :class="textColor">{{ message }}</p>
             </div>
+            <button @click="dismiss" type="button" :class="closeColour" class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5  inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800  dark:hover:bg-gray-700"  aria-label="Close">
+              <span class="sr-only">Close</span>
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+              </svg>
+            </button>
         </div>
     </div>
 </template>
@@ -23,7 +29,7 @@ import {
     ExclamationCircleIcon,
     ExclamationTriangleIcon,
 } from '@heroicons/vue/20/solid'
-import {ref, watchEffect} from 'vue'
+import {ref, watchEffect, defineEmits} from 'vue'
 
 const props = defineProps({
     message: String,
@@ -34,6 +40,12 @@ const props = defineProps({
             ['info', 'success', 'danger', 'warning'].includes(value),
     },
 })
+
+const emit = defineEmits(['dismiss'])
+
+function dismiss(){
+  emit('dismiss')
+}
 
 const icons = {
     info: InformationCircleIcon,
@@ -47,21 +59,26 @@ const colors = {
         background: 'bg-blue-50',
         icon: 'text-blue-400',
         text: 'text-blue-700',
+        close: 'bg-blue-50 hover:bg-blue-200 text-blue-500 focus:ring-blue-400 dark:text-blue-400',
     },
     success: {
         background: 'bg-green-50',
         icon: 'text-green-400',
         text: 'text-green-700',
+        close: 'bg-green-50 hover:bg-green-200 text-green-500 focus:ring-green-400 dark:text-green-400',
+
     },
     danger: {
         background: 'bg-red-50',
         icon: 'text-red-400',
         text: 'text-red-700',
+        close: 'bg-red-50 hover:bg-red-200 text-red-500 focus:ring-red-400 dark:text-red-400',
     },
     warning: {
         background: 'bg-yellow-50',
         icon: 'text-yellow-400',
         text: 'text-yellow-700',
+        close: 'bg-yellow-50 hover:bg-yellow-200 text-yellow-500 focus:ring-yellow-400 dark:text-yellow-400',
     },
 }
 
@@ -69,11 +86,12 @@ const iconComponent = ref(icons[props.type])
 const messageClass = ref(colors[props.type].background + ' rounded-md p-4')
 const iconColor = ref(colors[props.type].icon)
 const textColor = ref(colors[props.type].text)
-
+const closeColour = ref(colors[props.type].close)
 watchEffect(() => {
     iconComponent.value = icons[props.type]
     messageClass.value = colors[props.type].background + ' rounded-md p-4'
     iconColor.value = colors[props.type].icon
     textColor.value = colors[props.type].text
+    closeColour.value = colors[props.type].close
 })
 </script>

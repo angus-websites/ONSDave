bf85f31d0a93d023cd0e6d74ce4e8bedf639d201
Angus <angus.goody@gmail.com>
2023-11-11 19:45:12 +0000

    Added a reset time button to timePicker

--- resources/js/Components/TimePicker.vue
+++ resources/js/Components/TimePicker.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex flex-col items-center justify-center p-8 bg-gray-100 rounded-lg shadow-lg w-full lg:w-3/4 max-w-2xl"
+        class="relative flex flex-col items-center justify-center p-8 bg-gray-100 rounded-lg shadow-lg w-full lg:w-3/4 max-w-2xl"
     >
         <div class="flex items-center space-x-6">
             <!-- Hour Selector -->
@@ -49,6 +49,13 @@
                 </button>
             </div>
         </div>
+
+      <button v-if="manualInteraction" @click="resetTime" type="button" class="absolute right-4 top-1/2 transform -translate-y-1/2 text-zinc-600 bg-zinc-200 hover:bg-zinc-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+        <span class="sr-only">Reset time</span>
+      </button>
     </div>
 </template>
 
@@ -92,6 +99,17 @@ const updateTime = () => {
         updateTimeEmit()
     }
 }
+
+const resetTime = () => {
+  /**
+   * Reset the time to the current time
+   */
+  const now = new Date();
+  hours.value = now.getHours().toString().padStart(2, '0');
+  minutes.value = now.getMinutes().toString().padStart(2, '0');
+  manualInteraction.value = false; // Reset manual interaction flag
+  updateTimeEmit();
+};
 
 watch([hours, minutes], updateTimeEmit)
 

55b3e381bf43e45d4902bf84edbef17af168c3a6
Angus <angus.goody@gmail.com>
2023-11-12 23:28:31 +0000

    fixed session test

--- tests/Unit/DTOs/SessionTest.php
+++ tests/Unit/DTOs/SessionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\DTOs\Session;
 use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 
@@ -13,17 +14,35 @@ class SessionTest extends TestCase
     public function test_session_returns_correct_duration_in_seconds_for_one_hour(): void
     {
 
-        $now = Carbon::parse('2023-04-15 09:00:00');
+        $clockIn = Carbon::parse('2023-04-15 09:00:00');
+        $clockOut = Carbon::parse('2023-04-15 10:00:00');
 
-        $session = new \App\DTOs\Session(
-            clockIn: $now,
-            clockOut: $now->addHours(1),
-            duration: new \DateInterval('PT1H'),
+        $session = new Session(
+            clockIn: $clockIn,
+            clockOut: $clockOut,
             ongoing: false,
             autoClockOut: false,
         );
 
         $this->assertEquals(3600, $session->getDurationInSeconds());
+    }
+
+    /**
+     * Test a session returns correct duration in seconds for more complex duration.
+     */
+    public function test_session_returns_correct_duration_in_seconds_for_more_complex_duration(): void
+    {
+        $clockIn = Carbon::parse('2023-04-15 14:30:00');
+        $clockOut = Carbon::parse('2023-04-15 19:09:52');
+
+        $session = new Session(
+            clockIn: $clockIn,
+            clockOut: $clockOut,
+            ongoing: false,
+            autoClockOut: false,
+        );
+
+        $this->assertEquals(16792, $session->getDurationInSeconds());
     }
 
 }

0b474efd81e3c3ab94ceba167ba3a29a33b461b5
Angus <angus.goody@gmail.com>
2023-11-13 18:33:53 +0000

    Adding new api routes to fetch sessions

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function fetchDaySessions(Request $request)
+    {
+        // Logic to fetch sessions for a specific day
+    }
+
+    public function fetchMonthSessions(Request $request)
+    {
+        // Logic to fetch sessions for a specific month
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/sessions/day', [SessionController::class, 'fetchDaySessions'])->name('api.sessions.day');
+    Route::post('/sessions/month', [SessionController::class, 'fetchMonthSessions'])->name('api.sessions.month');
 });

ad10a1071d4f67ab5836a919fd94403805ccd577
Angus <angus.goody@gmail.com>
2023-11-13 23:13:07 +0000

    Added new leave migrations

---
+++ database/migrations/2023_11_13_230100_create_leave_types_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('from_balance')->default(false); // Does this leave type deduct from the employee's leave balance?
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_types');
+    }
+};
---
+++ database/migrations/2023_11_13_230215_create_leaves_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('leave_type_id')
+                ->references('id')
+                ->on('leave_types')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaves');
+    }
+};

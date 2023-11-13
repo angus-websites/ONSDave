af9ed8507d6c20a168b0a2e00a343df1d13340cf
Angus <angus.goody@gmail.com>
2023-11-13 00:01:23 +0000

    Created DaySessions abstract class

---
+++ app/DTOs/DaySessionsCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DTOs;
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use JsonSerializable;
+
+abstract class DaySessionsCollection implements JsonSerializable
+{
+    /**
+     * @param  Carbon  $date [Date]
+     * @param  Collection<DaySessions>  $days [Session]
+     */
+    public function __construct(
+        public Carbon $date,
+        public Collection $days,
+    ) {
+    }
+
+
+    public abstract function toArray(): array;
+
+    /**
+     * Get a DaySessions object for a given date
+     *
+     */
+    public function getDay(Carbon $date): ?DaySessions
+    {
+        return $this->days->first(fn (DaySessions $day) => $day->date->isSameDay($date));
+    }
+
+    /**
+     * Create a MonthSessions object from an array
+     * @param array $data
+     * @return MonthSessions
+     */
+    public static function fromArray(array $data): MonthSessions
+    {
+        return new MonthSessions(
+            $data['date'],
+            $data['days'],
+        );
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+}
